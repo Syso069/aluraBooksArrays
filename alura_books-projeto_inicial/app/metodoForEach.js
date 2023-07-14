1d08ca9681f989286fa4e40c640307cde679513d
Syso <sisomd69@gmail.com>
2023-07-14 12:03:49 -0300

    ordenando lista com sort e  indisponivel

--- alura_books-projeto_inicial/app/metodoForEach.js
+++ alura_books-projeto_inicial/app/metodoForEach.js
@@ -3,9 +3,10 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livros => {
+        let disponibilidade = livros.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${livros.imagem}" />
+            <img class="${disponibilidade}" src="${livros.imagem}" />
             <h2 class="livro__titulo">${livros.titulo}</h2>
             <p class="livro__descricao">${livros.autor}</p>
             <p class="livro__preco" id="preco">${livros.preco.toFixed(2)}</p>
